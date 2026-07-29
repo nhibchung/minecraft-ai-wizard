cd1ce847fdc11f3a3995e1161704317103cf02b2
NC <nhibchung@gmail.com>
2026-07-29 11:44:21 -0700

    Update CORS

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ app = FastAPI(title="Minecraft AI Wizard")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
